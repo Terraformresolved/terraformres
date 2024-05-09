add7b798c57accd683ff0be8335e374f8404c06f
Sugandasree <107237480+Sugayuv1@users.noreply.github.com>
2024-05-09 16:55:28 +0530

    Update cloudsql.tf

--- google_cloud/camunda/cloudsql.tf
+++ google_cloud/camunda/cloudsql.tf
@@ -1,17 +1,3 @@
-resource "google_sql_database_instance" "camunda-db" {
-  name             = "camunda-db-postgres"
-  database_version = "POSTGRES_11"
-  region           = local.config.region
-
-  settings {
-    # Very small instance for testing.
-    tier = "db-f1-micro"
-    ip_configuration {
-        ipv4_enabled = true
-    }
-  }
-}
-
 resource "google_sql_user" "user" {
   name     = "camunda"
   instance = google_sql_database_instance.camunda-db.name
